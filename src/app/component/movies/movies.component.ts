687b19b4f9320ee8db3791041447f6bc030c8cfb
Hugo Arcal <hugo@parisfashionshops.fr>
2022-07-06 16:16:07 +0200

    factorise pagination

--- src/app/component/movies/movies.component.ts
+++ src/app/component/movies/movies.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { MoviesService } from 'src/app/service/movies.service';
 import { Movie } from 'src/app/model/movie';
-import { FormControl } from '@angular/forms';
 
 
 @Component({
@@ -9,20 +8,12 @@ import { FormControl } from '@angular/forms';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent implements OnInit, OnChanges {
+export class MoviesComponent{
 
   @Input() movieList:Movie[] | undefined;
   @Input() searchMovie:any;
   page: number;
 
   constructor(private service: MoviesService) { }
-
-  ngOnInit(): void {  
-  }
-  ngOnChanges(){
-    if(this.searchMovie != undefined){
-      let data = this.service.findMovieByName(this.searchMovie);
-    }  
-  }
   
 }
